0728cf124db6df9677e95f8274d1b78edb5c6082
Jack Geary <jack.geary@ed.ac.uk>
2025-08-11 23:26:54 +0100

    Adding get_boundary_vals to linear model for plotting purposes

--- Clf_Learner/models/linear_model.py
+++ Clf_Learner/models/linear_model.py
@@ -17,6 +17,16 @@ class LinearModel(BaseModel, nn.Module):
         self.best_response = best_response
         self.loss = loss
 
+    def get_boundary_vals(self, X):
+        """(Optional) For the input 1-D X values, returns the y values that would lie
+            on the model decision boundary. This is only used for data visualisation (not included in repo)"""
+        W = self.fc.weight[0]
+        b = self.fc.bias
+
+        y = (-W[0]*X-b)*(1.0/W[1]) 
+        boundary_coords = torch.stack([X,y], dim=1)
+        return boundary_coords
+
     def get_weights(self, include_bias=True):
         weights = self.fc.weight
         if include_bias:
